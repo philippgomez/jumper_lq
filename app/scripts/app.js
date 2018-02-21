6d1ae52732058a522bcd275e236e3ebbb3872c0a
Philipp Gomez <mkchaz@gmail.com>
2018-02-21 21:55:20 +0800

    Added AngularJS structure

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var JumperLQ = angular.module('JumperLQ', ['ngRoute']);
+
+JumperLQ.factory('myHttpInterceptor', function($rootScope, $q) {
+  return {
+    'requestError': function(config) {
+      $rootScope.status = 'HTTP REQUEST ERROR ' + config;
+      return config || $q.when(config);
+    },
+    'responseError': function(rejection) {
+      $rootScope.status = 'HTTP RESPONSE ERROR ' + rejection.status + '\n' +
+                          rejection.data;
+      return $q.reject(rejection);
+    },
+  };
+});
+
+JumperLQ.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    controller : 'LoginCtrl',
+    templateUrl: '/partials/login.html',
+  });
+  $routeProvider.otherwise({
+    redirectTo : '/'
+  });
+});
+
+JumperLQ.config(function($httpProvider) {
+  $httpProvider.interceptors.push('myHttpInterceptor');
+});
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import json
 import webapp2
 
-
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
-
-
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
 ], debug=True)
